7ef7a4442ec8bf4ca047e75e5ca02d9ca89fc44d
hsu-git <hsu080206@gmail.com>
2025-04-09 16:44:22 +0900

    ..

--- src/main/java/org/example/bootthymeleaf/controller/MainController.java
+++ src/main/java/org/example/bootthymeleaf/controller/MainController.java
@@ -27,29 +27,27 @@ public class MainController {
 //        word.setText("고양이");
 //        wordRepository.save(word);
 //        model.addAttribute("words", wordRepository.findAll());
-        // 방법1. sort한다 (추천은 안함. 간단한 가설 검증)
+        // 방법 1. sort한다 (추천은 안함. 간단한 가설 검증)
 //        model.addAttribute("words",
 //                wordRepository.findAll().stream()
-                // 뒤집는 방법 1. : reverse하는 옵션이 어딘가에 있으니까 그걸 쓰세요.(는 권장안함)
-                // -> for문을 할 때 뒤부터 세는 for문을 하세요...
-                // 뒤집는 방법 2. : 이런 식으로 패러미터를 바꿔 a, b -> b, a
-//                .sorted((b, a) -> a.getCreatedAt().compareTo(b.getCreatedAt())).toList());
-                // 뒤집는 방법 3. : -를 붙인다 (boolean이면 not(!)을 하고)
-//                 .sorted((b, a) -> -a.getCreatedAt().compareTo(b.getCreatedAt())).toList());  // 오름차순 -> 데이터가 등장하는 방향과 데이터가 커지는 방향이 같을 때
-                // ascending <-> descending (내림차순)
-                // -> 역전(reverse <- 가장 쉽긴한데.. 이러지 마시고...
+//                    .sorted((a, b) -> a.getCreatedAt().compareTo(b.getCreatedAt()))
+        // 뒤집는 방법 1 : reverse하는 옵션이 어딘가에 있으니까 그걸 쓰세요(는 권장안함)
+        // -> for문을 할 때 뒤부터 세는 for문을 하세요...
+        // 뒤집는 방법 2 : 이런 식으로 패러미터를 바꿔 a, b -> b, a
+//                    .sorted((b, a) -> a.getCreatedAt().compareTo(b.getCreatedAt()))
+        // 뒤집는 방법 3 : -를 붙인다 (boolean이면 not(!)을 하고)
+//                    .sorted((a, b) -> -a.getCreatedAt().compareTo(b.getCreatedAt()))
+//                    .toList()); // 오름차순. -> 데이터가 등장하는 방향과 데이터가 커지는 방향이 같을 때
+        // ascending <-> descending (내림차순)
         // (스트림화) 앞에와 뒤를 불러와서 생성일자를 비교해 그걸로 정렬해 그리고 그걸 다시 리스트로 만들어
-        // -> 모젤로 전달
+        // -> 모델로 전달
+        // model.addAttribute("message", message);
 
-        // 방법 2. 쿼리 같은 걸 만들어줘야 하는데.. 기준을 createAt으로 잡아야겠네?
-        // (.....................uuid v4, uuid v6..............혼자 TIL 해보기)
+        // 방법2. 쿼리 같은 걸 만들어줘야하는데... 기준을 createdAt으로 잡아야겠네?
         model.addAttribute("words",
                 wordRepository.findAllByOrderByCreatedAtDesc());
-
-//        model.addAttribute("message", message);
-//        model.addAttribute("data", "Hello World!");
-        // 타임리프에서 이미 폼을 이미 정의된 걸로 쓰려면 Model을 통해 전달해야함.
-        model.addAttribute("wordForm", new WordForm()); // 주입함
+        // 타임리프에서 이미 폼을 이미 정의된 걸로 쓰려면 Model을 통해서 전달해야합니다
+        model.addAttribute("wordForm", new WordForm()); // 주입함!
         return "index"; // forward
     }
 
@@ -64,28 +62,21 @@ public class MainController {
 
     @PostMapping("/update")
     public String updateWord(@ModelAttribute UpdateWordForm form, RedirectAttributes redirectAttributes) {
-        // JPA는 업데이트용 메서드나 기능이 따로 없음
-        // JPA는 수정용이 따로 없음
-        // -> 교체 개념 => put <-> patch : 멱등성 (TIL)
+        // JPA는 업데이트용 메서드나 기능이 따로 없어요
+        // JPA는 수정용이 따로 없어요
+        // -> 교체 개념이에요 => put <-> patch : 멱등성 (TIL)
         // JPA : Jakarta Persistence API
-//        Word word = new Word();
-//        word.setText(form.getNewWord());
-//        word.setUuid(form.getUuid());
-        // -> 어? 너 이미 있는 애와 같은 이름으로 저장하려고 하는거지?
         Word oldWord = wordRepository.findById(form.getUuid()).orElseThrow();
-        // 없으면 에러처리하겠다 -> null로 인한 이슈를 깔끔하게 에러로 해버리겠다.
         oldWord.setText(form.getNewWord());
         wordRepository.save(oldWord);
-        // 너.. 원래 있던 애를 불러서 바꿔서 저장하는구나! OK!!
-        redirectAttributes.addFlashAttribute(
-                "message", "정상적으로 교체되었습니다. %s".formatted(oldWord.getUuid()));
+        redirectAttributes.addFlashAttribute("message", "정상적으로 교제되었습니다. %s".formatted(oldWord.getUuid()));
         return "redirect:/";
     }
 
     @PostMapping("/delete")
     public String deleteWord(@RequestParam("id") String uuid, RedirectAttributes redirectAttributes) {
         wordRepository.deleteById(uuid);
-        redirectAttributes.addFlashAttribute("message", "정상적으로 삭제되었습니다.%s".formatted(uuid));
+        redirectAttributes.addFlashAttribute("message", "정상적으로 삭제되었습니다. %s".formatted(uuid));
         return "redirect:/";
     }
 
@@ -93,13 +84,13 @@ public class MainController {
     public String resetWords(RedirectAttributes redirectAttributes) {
         wordRepository.deleteAll(); // 고양이들아 안녕!
         // Model을 쓸 수 없는 이유 -> forward
-//        redirectAttributes.addAttribute("message", "전체 삭제되었습니다.");
+//        redirectAttributes.addAttribute("message", "전체 삭제되었습니다!");
         // 주소창을 통해서 전달했기에 Parameter로 해서 받아서 Model로 넣어줘야했다면...
-//        redirectAttributes.addFlashAttribute("message2", "전부 DELETE 되었습니다.");
-        redirectAttributes.addFlashAttribute("message", "Reset words successfully");
+//        redirectAttributes.addFlashAttribute("message2", "전부 DELETE 되었습니다!");
+        redirectAttributes.addFlashAttribute("message", "끝말잇기 기록 리셋");
         // 바로 model로 넣어준다...
         // message -> Parameter
-        // 주소 창에 노출이 된다.
+        // 주소 창에 노출이 된다
         return "redirect:/";
     }
 }
