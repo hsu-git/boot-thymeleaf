716b3cc1da1f2153ac289a484f177e98b3167967
hsu-git <hsu080206@gmail.com>
2025-04-09 12:25:33 +0900

    reset

--- src/main/java/org/example/bootthymeleaf/controller/MainController.java
+++ src/main/java/org/example/bootthymeleaf/controller/MainController.java
@@ -6,6 +6,9 @@ import org.example.bootthymeleaf.model.repository.WordRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RequiredArgsConstructor
 @Controller
@@ -13,12 +16,27 @@ public class MainController {
     private final WordRepository wordRepository;
 
     @GetMapping
-    public String index(Model model) {
-        Word word = new Word();
-        word.setText("고양이");
-        wordRepository.save(word);
+    public String index(Model model, @RequestParam(required = false) String message) {
+//        Word word = new Word();
+//        word.setText("고양이");
+//        wordRepository.save(word);
         model.addAttribute("words", wordRepository.findAll());
+//        model.addAttribute("message", message);
 //        model.addAttribute("data", "Hello World!");
-        return "index";
+        return "index"; // forward
+    }
+
+    @PostMapping("/reset")
+    public String resetWords(RedirectAttributes redirectAttributes) {
+        wordRepository.deleteAll(); // 고양이들아 안녕!
+        // Model을 쓸 수 없는 이유 -> forward
+//        redirectAttributes.addAttribute("message", "전체 삭제되었습니다.");
+        // 주소창을 통해서 전달했기에 Parameter로 해서 받아서 Model로 넣어줘야했다면...
+//        redirectAttributes.addFlashAttribute("message2", "전부 DELETE 되었습니다.");
+        redirectAttributes.addFlashAttribute("message", "Reset words successfully");
+        // 바로 model로 넣어준다...
+        // message -> Parameter
+        // 주소 창에 노출이 된다.
+        return "redirect:/";
     }
 }
