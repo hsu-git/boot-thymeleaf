a1b02cfd7d501e134f0f898fe074e5ad0f2599cb
hsu-git <hsu080206@gmail.com>
2025-04-09 14:47:57 +0900

    delete

--- src/main/java/org/example/bootthymeleaf/controller/MainController.java
+++ src/main/java/org/example/bootthymeleaf/controller/MainController.java
@@ -7,6 +7,7 @@ import org.example.bootthymeleaf.model.repository.WordRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -56,6 +57,13 @@ public class MainController {
         Word word = new Word();
         word.setText(wordForm.getWord());
         wordRepository.save(word);
+        return "redirect:/";
+    }
+
+    @PostMapping("/delete")
+    public String deleteWord(@RequestParam("id") String uuid, RedirectAttributes redirectAttributes) {
+        wordRepository.deleteById(uuid);
+        redirectAttributes.addFlashAttribute("message", "정상적으로 삭제되었습니다.%s".formatted(uuid));
         return "redirect:/";
     }
 
